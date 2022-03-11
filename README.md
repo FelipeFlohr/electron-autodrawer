01a29b0047f055a7adbbebb42c067322493fe588
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-03-11 00:23:35 -0300

    Added animations and Fixed Initial Nav

---
+++ README.md
@@ -0,0 +1,3 @@
+# Electron Autodrawer
+
+W.I.P
--- src/view/assets/ts/navegation.ts
+++ src/view/assets/ts/navegation.ts
@@ -1,41 +1,41 @@
 // Navegation used for keeping the SPA
-// Self invoked
-(function () {
-    function navThroughAjax(hash: string) {
-        if (!hash) return
 
-        const link = document.querySelector(`[rPage="${hash}"]`)
-        if (!link) return
+function navThroughAjax(hash: string) {
+    if (!hash) return
 
-        const destiny = document.querySelector("main")
+    const link = document.querySelector(`[rPage="${hash}"]`)
+    if (!link) return
 
-        // Gets the destiny page content using Fetch API
-        const url = hash.substring(1)
-        fetch(url)
-            .then(resp => resp.text())
-            .then(html => {
-                destiny.innerHTML = html
-            })
+    const destiny = document.querySelector("main")
+
+    // Gets the destiny page content using Fetch API
+    const url = hash.substring(1)
+    fetch(url)
+        .then(resp => resp.text())
+        .then(html => {
+            destiny.innerHTML = html
+        })
+}
+
+function configLinks() {
+    document.querySelectorAll("[rPage]")
+        .forEach(link => {
+            link.setAttribute("href", link.attributes["rPage"].value)
+        })
+}
+
+function initialNav() {
+    if (location.hash) {
+        navThroughAjax(location.hash)
+        console.log("caiu aqui1. Location: " + location.hash)
+    } else {
+        const firstLink = document.querySelector("[rPage]")
+        console.log("caiu aqui. firstLink: " + firstLink.getAttribute("rPage"))
+        navThroughAjax(firstLink.getAttribute("rPage"))
     }
+}
 
-    function configLinks() {
-        document.querySelectorAll("[rPage]")
-            .forEach(link => {
-                link.setAttribute("href", link.attributes["rPage"].value)
-            })
-    }
+window.onhashchange = (event: any) => navThroughAjax(location.hash)
 
-    function initialNav() {
-        if (location.hash) {
-            navThroughAjax(location.hash)
-        } else {
-            const firstLink = document.querySelector("[rPage]")
-            navThroughAjax(firstLink.getAttribute("hash"))
-        }
-    }
-
-    window.onhashchange = event => navThroughAjax(location.hash)
-
-    configLinks()
-    initialNav()
-})()
+configLinks()
+initialNav()
